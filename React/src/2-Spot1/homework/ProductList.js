0d0ef900a78b3c17cb34c81111a0286ee965624a
Nahuel Rojas <nadaro@outlook.com>
2022-04-19 17:14:24 -0300

    ejercitacion react

--- React/src/2-Spot1/homework/ProductList.js
+++ React/src/2-Spot1/homework/ProductList.js
@@ -1,17 +1,4 @@
 import React,{useId} from 'react';
 
 
-const ProductList =({products})=>{
- 
-return <>{products.map(({nombre,color}) => {
-  return (
-		<section key={useId()}>
-			<h1>{nombre}</h1>
-			<h4>{color.map((x) => x.colors)}</h4>
-			<p>Disponible : {color[0].disponible ? 'si': 'no'}</p>
-		</section>
-	);
-})}</>;
-}
-
 export default ProductList
